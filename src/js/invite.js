fb45df747fac004223f8bf3d5a9d2451808b6de7
mrLuisFer <lolesuncrak@gmail.com>
2020-10-12 17:54:05 -0500

    Functionality in javascript added

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,8 +1,16 @@
 const iconDown = document.querySelector("#invite-icon-down");
 const invite_toShow = document.querySelector("#invite-toShow");
+const invite_title = document.querySelector("#invite-title");
 
 iconDown.addEventListener("click", () => {
   console.log("works for the moment");
 
   invite_toShow.classList.toggle("hidde");
+  if (invite_toShow.classList.contains("hidde")) {
+    invite_title.style.fontWeight = "400";
+    iconDown.style.transform = "rotate(360deg)";
+  } else {
+    invite_title.style.fontWeight = "700";
+    iconDown.style.transform = "rotate(180deg)";
+  }
 });
