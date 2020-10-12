908ecfb2223bf550d4732a1177f890b76a08104a
mrLuisFer <lolesuncrak@gmail.com>
2020-10-12 17:17:46 -0500

    First commit

---
+++ src/js/invite.js
@@ -0,0 +1,6 @@
+const iconDown = document.querySelector("#invite-icon-down");
+const invite_toShow = document.querySelector("#invite-toShow");
+
+iconDown.addEventListener("click", () => {
+  console.log("works for the moment");
+});
