b39a5ada9a8c499fba81566787d030fbea32ab37
mrLuisFer <lolesuncrak@gmail.com>
2020-10-14 12:42:24 -0500

    [FIX] Error in module of javascript file fixed

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,27 +1,8 @@
-const inviteIconDown = document.querySelector("#invite-icon-down");
+const invite_container = document.querySelector("#invite-container");
 const invite_toShow = document.querySelector("#invite-toShow");
 const invite_title = document.querySelector("#invite-title");
-const invite_container = document.querySelector("#invite-container");
+const inviteIconDown = document.querySelector("#invite-icon-down");
 
-// const handleToShow = require("./toShowFunction");
-import handleToShow from "./toShowFunction";
+import handleToShow from "./toShowFunction.js";
 
 handleToShow(invite_container, invite_toShow, invite_title, inviteIconDown);
-
-// invite_container.addEventListener("click", () => {
-//   console.log("works for the moment");
-
-//   invite_toShow.classList.toggle("hidde");
-//   if (invite_toShow.classList.contains("hidde")) {
-//     invite_title.style.fontWeight = "400";
-//     iconDown.style.transform = "rotate(360deg)";
-//   } else {
-//     invite_title.style.fontWeight = "700";
-//     iconDown.style.transform = "rotate(180deg)";
-//     invite_toShow.classList.add(
-//       "animate__animated",
-//       "animate__slideInDown",
-//       "animate__faster"
-//     );
-//   }
-// });
---
+++ src/js/maximum.js
@@ -0,0 +1,8 @@
+const maximum_container = document.querySelector("#maximum-container");
+const maximum_toShow = document.querySelector("#maximum-toShow");
+const maximum_title = document.querySelector("#maximum-title");
+const maximumIconDown = document.querySelector("#maximum-icon-down");
+
+import handleToShow from "./toShowFunction.js";
+
+handleToShow(maximum_container, maximum_toShow, maximum_title, maximumIconDown);
