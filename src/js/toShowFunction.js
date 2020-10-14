8a9efa724b6678ae3e25e36c85bf2b456b7f5706
mrLuisFer <lolesuncrak@gmail.com>
2020-10-13 23:05:46 -0500

    [FIX] Fixing some errors in Javascript

---
+++ src/js/toShowFunction.js
@@ -0,0 +1,18 @@
+export default function handleToShow(container, toShow, elemTitle, iconDown) {
+  container.addEventListener("click", () => {
+    toShow.classList.toggle("hidde");
+
+    if (toShow.classList.contains("hidde")) {
+      elemTitle.style.fontWeight = "400";
+      iconDown.style.transform = "rotate(360deg)";
+    } else {
+      elemTitle.style.fontWeight = "700";
+      iconDown.style.transform = "rotate(180deg)";
+      toShow.classList.add(
+        "animate__animated",
+        "animate__slideInDown",
+        "animate__faster"
+      );
+    }
+  });
+}
