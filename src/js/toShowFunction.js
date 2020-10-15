a33472d458b3c13ad7e38be2e4def825dc4cfe83
mrLuisFer <lolesuncrak@gmail.com>
2020-10-14 21:31:35 -0500

    [FIX] Fixing the javascript functionality and the wrapper styles

--- src/js/toShowFunction.js
+++ src/js/toShowFunction.js
@@ -13,16 +13,33 @@ export default function handleToShow(container, toShow, elemTitle, iconDown) {
         "animate__slideInDown",
         "animate__faster"
       );
+    }
+  });
 
-      window.addEventListener("click", (event) => {
-        console.log(event.target);
+  iconDown.addEventListener("click", () => {
+    toShow.classList.toggle("hidde");
 
-        if (event.target != container) {
-          toShow.classList.add("hidde");
-          elemTitle.style.fontWeight = "400";
-          iconDown.style.transform = "rotate(360deg)";
-        }
-      });
+    if (toShow.classList.contains("hidde")) {
+      elemTitle.style.fontWeight = "400";
+      iconDown.style.transform = "rotate(360deg)";
+    } else {
+      elemTitle.style.fontWeight = "700";
+      iconDown.style.transform = "rotate(180deg)";
+      toShow.classList.add(
+        "animate__animated",
+        "animate__slideInDown",
+        "animate__faster"
+      );
+    }
+  });
+
+  window.addEventListener("click", (event) => {
+    console.log(event.target);
+
+    if (event.target != container && event.target != elemTitle) {
+      toShow.classList.add("hidde");
+      elemTitle.style.fontWeight = "400";
+      iconDown.style.transform = "rotate(360deg)";
     }
   });
 }
